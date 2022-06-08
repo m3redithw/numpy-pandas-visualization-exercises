9940d42d7b25995a53e5f92805bab866eb42ec4e
Meredith Wang <wang.meredith09@gmail.com>
2022-06-08 10:59:33 -0500

    added advanced dataframes exercises part 2

--- advanced_dataframes.py
+++ advanced_dataframes.py
@@ -1,3 +1,4 @@
+# Exercise 1
 # 1. Run python -m pip install pymysql from your terminal to install the mysql client (any folder is fine)
 
 # 2. cd into your exercises folder for this module and run echo env.py >> .gitignore
@@ -17,13 +18,11 @@ pd.read_sql('SELECT * FROM employees LIMIT 5 OFFSET 50', get_db_url)
 ## a. Intentionally make a typo in the database url. What kind of error message do you see?
 
 pd.read_sql('SELECT * FROM employees LIMIT 5 OFFSET 50', get_db_url)
-
 # OperationalError
 
 ## b. Intentionally make an error in your SQL query. What does the error message look like?
 
 pd.read_sql('SELECT FROM employees LIMIT 5 OFFSET 50', get_db_url)
-
 # ProgrammingError
 
 # 6. Read the employees and titles tables into two separate DataFrames.
@@ -79,7 +78,6 @@ titles.describe(include = 'all')
 # 9. How many unique titles are in the titles DataFrame?
 
 len(titles.groupby(by = 'title'))
-
 # there are 7 unique titles
 
 # 10. What is the oldest date in the to_date column?
@@ -96,3 +94,123 @@ titles.sort_values(by = 'to_date', ascending = False).head(1)
 #  	emp_no 	title 	from_date 	to_date
 # 0 	10001 	Senior Engineer 	1986-06-26 	9999-01-01
 ## now
+
+
+# ----------------------------------------------------------------
+# Exercise 2
+# 1. Copy the users and roles DataFrames from the examples above.
+
+users = pd.DataFrame({
+    'id': [1, 2, 3, 4, 5, 6],
+    'name': ['bob', 'joe', 'sally', 'adam', 'jane', 'mike'],
+    'role_id': [1, 2, 3, 3, np.nan, np.nan]
+})
+users
+
+roles = pd.DataFrame({
+    'id': [1, 2, 3, 4],
+    'name': ['admin', 'author', 'reviewer', 'commenter']
+})
+roles
+
+# 2. What is the result of using a right join on the DataFrames?
+
+users.merge(roles, left_on='role_id', right_on='id', how='right', indicator=True)
+#  	id_x 	name_x 	role_id 	id_y 	name_y 	_merge
+# 0 	1.0 	bob 	1.0 	1 	admin 	both
+# 1 	2.0 	joe 	2.0 	2 	author 	both
+# 2 	3.0 	sally 	3.0 	3 	reviewer 	both
+# 3 	4.0 	adam 	3.0 	3 	reviewer 	both
+# 4 	NaN 	NaN 	NaN 	4 	commenter 	right_only
+
+# 3. What is the result of using an outer join on the DataFrames?
+
+users.merge(roles, left_on='role_id', right_on='id', how='outer', indicator=True)
+#  	id_x 	name_x 	role_id 	id_y 	name_y 	_merge
+# 0 	1.0 	bob 	1.0 	1.0 	admin 	both
+# 1 	2.0 	joe 	2.0 	2.0 	author 	both
+# 2 	3.0 	sally 	3.0 	3.0 	reviewer 	both
+# 3 	4.0 	adam 	3.0 	3.0 	reviewer 	both
+# 4 	5.0 	jane 	NaN 	NaN 	NaN 	left_only
+# 5 	6.0 	mike 	NaN 	NaN 	NaN 	left_only
+# 6 	NaN 	NaN 	NaN 	4.0 	commenter 	right_only
+
+# 4. What happens if you drop the foreign keys from the DataFrames and try to merge them?
+
+
+# 5. Load the mpg dataset from PyDataset.
+
+from pydataset import data
+mpg = data('mpg')
+mpg
+
+# 6. Output and read the documentation for the mpg dataset.
+
+data('mpg', show_doc = True)
+
+# 7. How many rows and columns are in the dataset?
+
+mpg.shape
+# 234 columns x 11 rows
+
+# 8. Check out your column names and perform any cleanup you may want on them.
+
+mpg.columns
+# Index(['manufacturer', 'model', 'displ', 'year', 'cyl', 'trans', 'drv', 'cty',
+#        'hwy', 'fl', 'class'],
+#       dtype='object')
+
+# 9. Display the summary statistics for the dataset.
+
+mpg.describe()
+#  	displ 	year 	cyl 	cty 	hwy
+# count 	234.000000 	234.000000 	234.000000 	234.000000 	234.000000
+# mean 	3.471795 	2003.500000 	5.888889 	16.858974 	23.440171
+# std 	1.291959 	4.509646 	1.611534 	4.255946 	5.954643
+# min 	1.600000 	1999.000000 	4.000000 	9.000000 	12.000000
+# 25% 	2.400000 	1999.000000 	4.000000 	14.000000 	18.000000
+# 50% 	3.300000 	2003.500000 	6.000000 	17.000000 	24.000000
+# 75% 	4.600000 	2008.000000 	8.000000 	19.000000 	27.000000
+# max 	7.000000 	2008.000000 	8.000000 	35.000000 	
+
+# 10. How many different manufacturers are there?
+
+len(mpg.groupby('manufacturer'))
+# 15
+
+# 11. How many different models are there?
+
+len(mpg.groupby('model'))
+# 38
+
+# 12. Create a column named mileage_difference like you did in the DataFrames exercises;
+# this column should contain the difference between highway and city mileage for each car.
+
+mpg['mileage_difference'] = mpg['hwy']-mpg['cty']
+
+# 13. Create a column named average_mileage like you did in the DataFrames exercises;
+# this is the mean of the city and highway mileage.
+
+mpg['average_mileage'] = (mpg['cty'] + mpg['hwy']) /2
+
+# 14. Create a new column on the mpg dataset named is_automatic that holds boolean values denoting whether the car has an automatic transmission.
+
+mpg['is_automatic'] = (mpg['trans'].str.contains('auto'))
+mpg.head()
+
+# 15. Using the mpg dataset, find out which which manufacturer has the best miles per gallon on average?
+
+bestmiles = mpg.groupby('manufacturer')[['average_mileage','manufacturer']].mean().sort_values(by='average_mileage', ascending = False)
+bestmiles.head()
+# honda
+
+# 16. Do automatic or manual cars have better miles per gallon?
+
+better = mpg.groupby('is_automatic')[['trans', 'average_mileage']].mean()
+better.head()
+# Yes
+
+# 	average_mileage
+# is_automatic 	
+# False 	22.227273
+# True 	19.130573
